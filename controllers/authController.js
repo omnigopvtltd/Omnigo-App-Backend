79730cf37d8e5d4e1205c5d2ace8a187bd8ef0c5
maryammuhammadafzal <natiqnagori7@gmail.com>
2026-08-13 12:47:37 +0500

    fix signup controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -563,7 +563,7 @@ exports.signup = [
         addresses: addresses || [],
       };
 
-      // Only add riderProfile if it was actually provided
+      // Only add riderProfile if it was provided
       if (riderProfile) {
         userData.riderProfile = {
           category: riderProfile.category || null,
