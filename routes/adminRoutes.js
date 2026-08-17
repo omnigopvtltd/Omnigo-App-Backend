39dff469a024e4221db434b1b17b2fd243f085ec
maryammuhammadafzal <natiqnagori7@gmail.com>
2026-08-17 16:15:05 +0500

    created new controller for notification and order

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -8,10 +8,12 @@ const {
   markAllNotificationsRead,
   getProfile,
   updateProfile,
+  markNotificationsRead,
 } = require("../controllers/adminController");
 
 router.get("/search", auth, role("admin"), globalSearch);
 router.get("/notifications", auth, getNotifications);
+router.patch("/update/notifications/read/:id", auth, markNotificationsRead);
 router.patch("/update/notifications/read-all", auth, role("admin"), markAllNotificationsRead);
 router.get("/profile/:id", auth, role("admin"), getProfile);
 router.put("/update/profile/:id", auth, role("admin"), updateProfile);
