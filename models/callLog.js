5dc9f609c2ff29d7672745b348b98401ff06b64a
maryammuhammadafzal <natiqnagori7@gmail.com>
2026-09-09 17:27:33 +0500

    fix callLog error

--- models/callLog.js
+++ models/callLog.js
@@ -75,7 +75,7 @@ const CallLogSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-callLogSchema.index({ callerId: 1, createdAt: -1 });
-callLogSchema.index({ receiverId: 1, createdAt: -1 });
+CallLogSchema.index({ callerId: 1, createdAt: -1 });
+CallLogSchema.index({ receiverId: 1, createdAt: -1 });
 
-module.exports = mongoose.model("CallLog", callLogSchema);
+module.exports = mongoose.model("CallLog", CallLogSchema);
